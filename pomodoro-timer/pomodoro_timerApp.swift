b8b01dba9a6d7603d5b935316fad1b4cc74afa66
Ryan Schofield <ryanjschofield2@gmail.com>
2025-07-19 10:04:41 -0500

    Initial Commit

---
+++ pomodoro-timer/pomodoro_timerApp.swift
@@ -0,0 +1,17 @@
+//
+//  pomodoro_timerApp.swift
+//  pomodoro-timer
+//
+//  Created by ryan schofield on 7/19/25.
+//
+
+import SwiftUI
+
+@main
+struct pomodoro_timerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
