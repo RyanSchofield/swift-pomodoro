2a6f1a55365c37455a87a29556088ce05401f3d6
Ryan Schofield <ryanjschofield2@gmail.com>
2025-07-24 14:45:01 -0500

    Allow user to set time intervals and length of cycle

--- pomodoro-timer/ContentView.swift
+++ pomodoro-timer/ContentView.swift
@@ -17,7 +17,6 @@ func getCountdownDisplay(remaining: Int, paused: Bool) -> String {
     let seconds: Int = remaining % 60
 
     let minutesDisp = String(minutes)
-    
     let secondsDisp = seconds >= 10 ? String(seconds) : "0" + String(seconds)
     
     var disp = "\(minutesDisp):\(secondsDisp)"
@@ -27,11 +26,6 @@ func getCountdownDisplay(remaining: Int, paused: Bool) -> String {
     return disp
 }
 
-let longInterval = 25 * 60
-let shortInterval = 5 * 60
-let cycleLength = 4
-//let longInterval = 12
-
 var audioPlayer: AVAudioPlayer?
 
 enum Status {
@@ -39,84 +33,122 @@ enum Status {
     case relax
 }
 
-
 struct ContentView: View {
+	@State var longInterval = 0
+	@State var shortInterval = 0
+	@State var cycleLength = 0
+	
     @State var status = Status.work
-    @State var timeRemaining = longInterval
+    @State var timeRemaining = 0
     @State var paused = false
     @State var message = "Pomodoro"
     @State var numCompleted = 0
+	
+	@State var longInput = 25
+	@State var shortInput = 5
+	@State var cycleInput = 4
+    
+    @State var ready = false
+    
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
-    
-    
-//    let display =
-
     var body: some View {
         
-        VStack {
-            Text(message)
-                .padding([.top], 30)
-                .padding([.bottom], 20)
-                .font(.system(size: 24))
-            
-            Text("completed: " + String(numCompleted))
-            
-            HStack {
-                if timeRemaining > 0 {
-                    Button(paused ? "resume" : "pause") {
-                        paused = !paused
-                    }
-                }
+        if ready {
+            VStack {
+                Text(message)
+                    .padding([.top], 30)
+                    .padding([.bottom], 20)
+                    .font(.system(size: 24))
                 
-                if timeRemaining <= 0 {
-                    Button("restart") {
-                        paused = true
-                        if status == Status.work {
-                            numCompleted += 1
-                            
-                            var newInterval = shortInterval
-                            if numCompleted % cycleLength == 0 && numCompleted > 0 {
-                                 newInterval = longInterval
+                Text("completed: " + String(numCompleted))
+                
+                HStack {
+                    if timeRemaining > 0 {
+                        Button(paused ? "resume" : "pause") {
+                            paused = !paused
+                        }
+                    }
+                    
+                    if timeRemaining <= 0 {
+                        Button("restart") {
+                            paused = true
+                            if status == Status.work {
+                                numCompleted += 1
+                                
+                                var newInterval = shortInterval
+                                if numCompleted % cycleLength == 0 && numCompleted > 0 {
+                                    newInterval = longInterval
+                                }
+                                status = Status.relax
+                                timeRemaining = newInterval
+                                
+                            } else {
+                                status = Status.work
+                                timeRemaining = longInterval
                             }
-                            status = Status.relax
-                            timeRemaining = newInterval
-                            
-                        } else {
-                            status = Status.work
-                            timeRemaining = longInterval
                         }
                     }
                 }
-                
+                Text(getCountdownDisplay(remaining: timeRemaining, paused: paused))
+                    .padding([.all], 40)
+                    .frame(width: 300, height: 300)
+                    .font(.system(size: 32))
+                    .onReceive(timer) { _ in
+                        if timeRemaining > 0 && !paused {
+                            timeRemaining -= 1
+                        }
+                        
+                        if timeRemaining == 0 {
+                            playSound(status)
+                            timeRemaining -= 1
+                        }
+                    }
             }
-            
-            
-            
-            Text(getCountdownDisplay(remaining: timeRemaining, paused: paused))
-                .padding([.all], 40)
-                .frame(width: 300, height: 300)
-                .font(.system(size: 32))
-                .onReceive(timer) { _ in
-                    if timeRemaining > 0 && !paused {
-                        timeRemaining -= 1
-                    }
-                    
-                    if timeRemaining == 0 {
-                        playSound(status)
-                        timeRemaining -= 1
-                    }
-                }
+        } else {
+			VStack {
+				HStack {
+					Text("Long Interval")
+					TextField("enter time", value: $longInput, format: .number)
+						.padding()
+						.frame(width: 150)
+				}
+				
+				HStack {
+					Text("Short Interval")
+					TextField("enter time", value: $shortInput, format: .number)
+						.padding()
+						.frame(width: 150)
+				}
+				
+				HStack {
+					Text("Cycle Length")
+					TextField("enter poms", value: $cycleInput, format: .number)
+						.padding()
+						.frame(width: 150)
+				}
+				
+				Button("Ready") {
+					if shortInput > longInput { return }
+					if shortInput <= 0 || longInput <= 0 {
+						return
+					}
+					
+					if cycleInput <= 0 { return }
+					shortInterval = shortInput * 60
+					longInterval = longInput * 60
+					timeRemaining = longInterval
+					cycleLength = cycleInput
+					ready  = true
+				}
+			}
         }
-        
-        
-        
     }
 }
-
+        
+     
 func playSound(_ status: Status) {
     let uri = status == Status.work ? "chill-chords-143504.mp3" : "trance-loop.mp3"
-
     
     guard let url = Bundle.main.url(forResource: uri, withExtension: nil) else {
             print("Sound file not found")
